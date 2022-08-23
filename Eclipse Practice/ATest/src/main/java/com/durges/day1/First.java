6d70d101ca827a694fe68985f0861bbe7853a469
Harshit <hvofficial2@gmail.com>
2022-08-24 02:35:55 +0530

    servlets

---
+++ Eclipse Practice/ATest/src/main/java/com/durges/day1/First.java
@@ -0,0 +1,20 @@
+package com.durges.day1;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import jakarta.servlet.GenericServlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+
+public class First extends GenericServlet {
+
+	@Override
+	public void service(ServletRequest req, ServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		out.println("Current Date and Time: "+(new Date()).toString());
+	}
+}
